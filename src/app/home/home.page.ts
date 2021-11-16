c33ab7637cd0ab8f0084af1711c46b4f43a131d8
Grant <grant.solomons@mezzanineware.com>
2021-11-17 01:21:30 +0200

    added card png's

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -10,25 +12,41 @@ export class HomePage {
   player1Name: string;
   player2Name: string;
   isStartScreen: boolean;
-  constructor() {
+  gameScreen: boolean;
+
+  constructor(
+    private alert: AlertController
+  ) {
 
   }
 
   ngOnInit() {
     
     this.isStartScreen = true
+    this.gameScreen = false
     console.log(this.isStartScreen);
+    this.player1Name  = "Grant"
+    this.player2Name = "Devon"
 
   }
 
   shownames() {
     if (this.player1Name != undefined && this.player2Name != undefined) {
       this.isStartScreen = false
+      this.gameScreen = true
       console.log(this.player1Name, this.player2Name)
     } else {
-      console.log("flippityfloppidy");
-      
+      this.presentAlert("Please make sure both player names are entered.")
     }
-    
+  }
+
+  async presentAlert(msg) {
+    const alert = await this.alert.create({
+      header: "Alert!",
+      message: msg,
+      buttons: ["okay"]
+    })
+
+    await alert.present()
   }
 }
